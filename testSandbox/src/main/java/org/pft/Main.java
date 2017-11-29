ddd1e34e26fdda95683cb107187a0abd6dad7c8f
sol1ps1st <alvi88@list.ru>
2017-11-29 14:46:47 +0300

    Add sand box

---
+++ testSandbox/src/main/java/org/pft/Main.java
@@ -0,0 +1,12 @@
+package org.pft;
+
+public class Main {
+    public static void main(String[] args){
+        char c1, c2;
+        String str = "ffuel";
+        c1 = str.charAt(0);
+        c2 = str.charAt(1);
+        System.out.println(c1 == c2);
+        System.out.println(c1 == c2);
+    }
+}
