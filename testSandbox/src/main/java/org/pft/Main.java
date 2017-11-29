848b32e8ff23bcb767c35eb017b747562e25ca74
Alex <alvi88@list.ru>
2017-11-29 21:15:12 +0300

    some work in sandbox Main

--- testSandbox/src/main/java/org/pft/Main.java
+++ testSandbox/src/main/java/org/pft/Main.java
@@ -1,5 +1,9 @@
 package org.pft;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
         char c1, c2;
@@ -8,5 +12,13 @@ public class Main {
         c2 = str.charAt(1);
         System.out.println(c1 == c2);
         System.out.println(c1 == c2);
+
+        String[] langs = {"java", "C#", "Delphi"};
+        for(String l: langs){
+            System.out.println(l);
+        }
+        List<String> list =  Arrays.asList("123","3");
+        List<String> list1 =  Arrays.asList(langs);
+        //ArrayList<int> d = new ArrayList<int>();
     }
 }
