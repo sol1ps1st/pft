dc1fb60d5f43e514d45aabd1ac96318dddfbf60a
sol1ps1st <alvi88@list.ru>
2017-11-30 13:11:09 +0300

    equals and hashcode add to class Query

--- test1/src/test/java/org/pft/model/Query.java
+++ test1/src/test/java/org/pft/model/Query.java
@@ -1,6 +1,28 @@
 package org.pft.model;
 
 public class Query {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Query)) return false;
+
+        Query query = (Query) o;
+
+        return queryText != null ? queryText.equals(query.queryText) : query.queryText == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return queryText != null ? queryText.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Query{" +
+                "queryText='" + queryText + '\'' +
+                '}';
+    }
+
     public Query(String queryText){
         setQueryText(queryText);
     }
